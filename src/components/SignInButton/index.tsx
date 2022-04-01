be80113e31b3f72b2aa6b9793f906aac512e992b
Luiz Sandoval (Americas) <luiz.sandoval@global.ntt>
2022-04-01 17:34:15 -0300

    feat: setup Next Auth with GitHub

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,22 +1,31 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 
+import { signIn, signOut, useSession } from 'next-auth/react';
+
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-    const isUserLoggedIn = true;
+    const { data: session } = useSession();
 
-    return isUserLoggedIn ? (
-        <button type='button' className={styles['sign-in-button']}>
+    return session ? (
+        <button
+            type='button'
+            className={styles['sign-in-button']}
+            onClick={() => signOut()}
+        >
             <FaGithub color='#04d361' />
-            Luiz Sandoval
-            <FiX color="#737380" className={styles['close-icon']}/>
+            {session.user.name}
+            <FiX color='#737380' className={styles['close-icon']} />
         </button>
-    ) :
-    (
-        <button type='button' className={styles['sign-in-button']}>
+    ) : (
+        <button
+            type='button'
+            className={styles['sign-in-button']}
+            onClick={() => signIn('github')}
+        >
             <FaGithub color='#eba417' />
             Sign in with Github
         </button>
-    )
+    );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,17 @@
 import { AppProps } from 'next/app';
 
+import { SessionProvider as NextAuthProvider } from 'next-auth/react';
+
 import '../styles/global.scss';
 
 import { Header } from '../components/Header';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <>
+        <NextAuthProvider session={pageProps.session}>
             <Header />
             <Component {...pageProps} />
-        </>
+        </NextAuthProvider>
     );
 }
 
